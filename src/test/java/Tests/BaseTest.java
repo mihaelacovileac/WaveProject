d3243a992b5411e22ec812b4648e977ac79bd0c2
mihaelacovileac <32909218+mihaelacovileac@users.noreply.github.com>
2017-11-12 16:30:08 -0800

    new

---
+++ src/test/java/Tests/BaseTest.java
@@ -0,0 +1,89 @@
+package Tests;
+
+import Pages.LoginPage;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import static org.testng.Assert.assertTrue;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+    protected LoginPage loginPage;
+
+    @Parameters({"browser", "url"})
+    @BeforeClass
+    public void baseBeforeClass(String browser,String url) throws MalformedURLException {
+        //Run FireFox
+        if (browser.equalsIgnoreCase("firefox")) {
+            driver = new FirefoxDriver();
+            driver.get(url);
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        }
+        //run Chrome
+        else if (browser.equalsIgnoreCase("chrome")) {
+            driver = new ChromeDriver();
+            driver.get(url);
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        }
+        //Run Appium
+        else if (browser.equalsIgnoreCase("appium")) {
+            //Create object of DesiredCapabilities class and specify android platform
+            DesiredCapabilities capabilities = DesiredCapabilities.android();
+            capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, BrowserType.CHROME);
+
+            capabilities.setCapability(MobileCapabilityType.PLATFORM, Platform.ANDROID);
+
+            capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+
+            capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Android");
+
+            capabilities.setCapability(MobileCapabilityType.VERSION, "6.0.1");
+
+            URL urlHub = new URL("http://127.0.0.1:4723/wd/hub");
+
+            driver = new AndroidDriver(urlHub, capabilities);
+
+            driver.get(url);
+
+            driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        } else if ((browser.equalsIgnoreCase("saucelab"))) {
+            String USERNAME = "mihaela04";
+            String ACCESS_KEY = "4263ab2a-bbcb-4348-a149-c95e9ecbcff1";
+            String URL = "https://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:443/wd/hub";
+
+            DesiredCapabilities caps = DesiredCapabilities.chrome();
+            caps.setCapability("platform", "Linux");
+            caps.setCapability("version", "47.0");
+            driver = new RemoteWebDriver(new URL(URL), caps);
+            driver.get(url);
+            driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        }
+    }
+    @AfterClass
+    public void baseAfterClass(){
+//        driver.close();
+//        driver.quit();
+    }
+    @Test
+    public void assertLogo(){
+        loginPage = new LoginPage(driver);
+        assertTrue(loginPage.getLogo().isDisplayed());}
+}
