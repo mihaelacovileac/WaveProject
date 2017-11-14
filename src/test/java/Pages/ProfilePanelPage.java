5f15da5d39f6d88b1ae52430a7e9c420ef30db79
mihaelacovileac <32909218+mihaelacovileac@users.noreply.github.com>
2017-11-13 17:36:59 -0800

    newPages

--- src/test/java/Pages/ProfilePanelPage.java
+++ src/test/java/Pages/ProfilePanelPage.java
@@ -9,7 +9,28 @@ public class ProfilePanelPage extends BasePage {
     public ProfilePanelPage(WebDriver driver){super(driver);}
     @FindBy(xpath = "//button[@ng-click='app.logout()']")
     private WebElement signOut;
+    @FindBy(xpath = "//em[@class='icon-user fa-lg']")
+    private WebElement userIcon;
+    @FindBy(xpath = "//em[@class='icon-equalizer fa-lg']")
+    private WebElement subscriptionIcon;
+    @FindBy(xpath = "//button[contains(.,'Edit Profile')]")
+    private WebElement editProfileBtn;
+    @FindBy(xpath = "//button[contains(.,'Edit Company')]")
+    private WebElement editCompanyBtn;
+
     public void clickSignOut(){
         wait.until(ExpectedConditions.visibilityOf(signOut)).click();
+    }
+    public void clickUserIcon(){
+        wait.until(ExpectedConditions.elementToBeClickable(userIcon)).click();
+    }
+    public void clickSubscriptionIcon(){
+        wait.until(ExpectedConditions.elementToBeClickable(subscriptionIcon)).click();
+    }
+    public void clickEditProfileBtn(){
+        wait.until(ExpectedConditions.elementToBeClickable(editProfileBtn)).click();
+    }
+    public void clickEditCompanyBtn(){
+        wait.until(ExpectedConditions.elementToBeClickable(editCompanyBtn)).click();
     }
 }
